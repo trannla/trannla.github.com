d7bae225653350101019d06bbf0f693e174c8542
trannla <trannla@gmail.com>
2015-04-01 19:53:28 -0400

    Added Slide and Scroll

---
+++ scrolling/files/js/utilities.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	$(function() {
+		$.scrollIt({
+			activeClass: 'active',
+			topOffset: 120
+		});
+	});
+
+});
---
+++ slideshow/files/js/utilities.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$('.flexslider').flexslider({
+		animation: 'slide'
+	});
+
+	$('.flexslider2').flexslider({
+		animation: 'slide',
+		controlNav: 'thumbnails'
+	});
+	
+});
